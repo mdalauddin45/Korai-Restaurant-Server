4cd87228220501e63e7d2110c54063e868c676cf
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-11-07 22:51:34 +0600

    user and pass env add

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 
+//MongoDb Add
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.teba24n.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 app.get("/", (req, res) => {
   res.send("assignment is running");
 });
